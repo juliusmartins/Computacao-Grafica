f143ff36002043936d89e99362bfb060d9ea0794
juliusmartins <juliusmla@hotmail.com>
2019-07-01 15:07:44 -0300

    Create Camera.cpp

---
+++ Camera.cpp
@@ -0,0 +1,19 @@
+#include <GL/glut.h>
+#include <stdlib.h>
+#include <math.h>
+
+float lx=0.0f,lz=-1.0f;
+
+float x=0.0f,z=7.0f;
+
+float door_angle=0.0f;
+
+float angle=0.0f;
+
+void init(){
+	glClearColor(0.0,0.7,1.0,1.0);
+}
+
+void drawHouse(){
+
+}
